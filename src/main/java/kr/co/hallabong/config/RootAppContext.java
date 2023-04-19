6b60ec38b1348052f29cae3fa6d0a16db6c7c241
Changwoo97 <106899977+Changwoo97@users.noreply.github.com>
2023-04-19 19:24:06 +0900

    Initial commit

---
+++ src/main/java/kr/co/hallabong/config/RootAppContext.java
@@ -0,0 +1,8 @@
+package kr.co.hallabong.config;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RootAppContext {
+
+}
---
+++ src/main/java/kr/co/hallabong/config/ServletAppContext.java
@@ -0,0 +1,28 @@
+package kr.co.hallabong.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@ComponentScan("kr.co.hallabong.controller") //스캔할 패키지
+@EnableWebMvc //Controller로 등록되어 있는 클래스 셋팅
+@Configuration
+public class ServletAppContext implements WebMvcConfigurer {
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// view 경로와 확장자 셋팅
+		WebMvcConfigurer.super.configureViewResolvers(registry);
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 이미지,영상, 소리 등 정적파일 경로 매핑
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+}
+
